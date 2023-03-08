5ccc7f266066d39786a81c9f17b7a6ba967f0fee
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-03-08 19:05:54 +0900

    #11 - 스프링 부트 프로젝트 시작
    초기의존성 
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ hana-study-board/src/main/java/com/example/hanastudyboard/HanaStudyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanastudyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaStudyBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaStudyBoardApplication.class, args);
+    }
+
+}
