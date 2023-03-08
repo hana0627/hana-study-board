ff46a7d15e2cbde033fcb2d6f13f4c260ce39187
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-03-08 19:05:54 +0900

    #6 - 스프링 부트 프로젝트 시작
    초기의존성 
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ hana-study-board/src/test/java/com/example/hanastudyboard/HanaStudyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanastudyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaStudyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
