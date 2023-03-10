5396674741fe108dfd6f275003709de17378ee61
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-03-10 19:51:50 +0900

    #21 - thymeleaf 3 decoupled logic 설정
    java code로 thymeleaf3 3 decoupled logic을 사용자 application property로 제어할 수 있게 설정 추가

---
+++ hana-study-board/src/main/java/com/example/hanastudyboard/config/ThymeleafConfig.java
@@ -0,0 +1,44 @@
+package com.example.hanastudyboard.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+
+/**
+ * 빨간줄 뜰때 해결법
+ * @ConfigurationProperties를 직접 선언했다면 main method에
+ * @ConfigurationPropertiesScan 붙여줘야함
+ * yml파일에
+ *   spring: thymeleaf3: decoupled-logic: true 설정 추가
+ */
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+
+        return defaultTemplateResolver;
+    }
+
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding
+    @ConfigurationProperties("spring.thymeleaf3")
+    public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
+        private final boolean decoupledLogic;
+    }
+
+}
